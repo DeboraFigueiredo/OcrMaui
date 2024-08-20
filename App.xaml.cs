4d2921146d76e3893e546e83a7e3056946c33311
DeboraFigueiredo <deborafigueiredoo26@gmail.com>
2024-08-20 17:13:31 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OcrMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
